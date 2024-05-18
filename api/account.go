ad61ba36eaf36641507d325e67f3379edbce27a3
next2golang <hidavidk@gmail.com>
2024-05-18 18:27:54 +1000

    feat(api-test): api test using mockdb

--- api/account.go
+++ api/account.go
@@ -36,7 +36,7 @@ func (server *Server) createAccount(ctx *gin.Context) {
 }
 
 type getAccountRequest struct {
-	ID int64 `uri:"id" binding="required,min=1"`
+	ID int64 `uri:"id" binding:"required,min=1"`
 }
 
 func (server *Server) getAccount(ctx *gin.Context) {
@@ -59,13 +59,13 @@ func (server *Server) getAccount(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, account)
 }
 
-type listAccountParams struct {
-	PageID   int32 `form:"page_id" binding="required,min=1"`
-	PageSize int32 `form:"page_size" binding="required,min=5,max=10"`
+type listAccountRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 }
 
 func (server *Server) listAccount(ctx *gin.Context) {
-	var req listAccountParams
+	var req listAccountRequest
 	if err := ctx.ShouldBindQuery(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
