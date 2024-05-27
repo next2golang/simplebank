3f1e7e9b4baf7e51140b2f539bfa1f73449902d5
next2golang <hidavidk777@gmail.com>
2024-05-27 06:12:30 -0400

    feat(sql): implement partial update with sqlc

--- db/query/user.sql
+++ db/query/user.sql
@@ -11,3 +11,13 @@ INSERT INTO users (
 -- name: GetUser :one
 SELECT * from users
 WHERE username = $1 LIMIT 1;
+
+-- name: UpdateUser :one
+Update users
+SET
+  hashed_password = coalesce(sqlc.narg(hashed_password), hashed_password),
+  full_name = coalesce(sqlc.narg(full_name), full_name),
+  email=coalesce(sqlc.narg(email), email)
+WHERE
+  username = sqlc.arg(username)
+RETURNING *;
